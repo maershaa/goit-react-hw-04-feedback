688ae977b35d8a60ea822343150358494ddd3e08
maershaa <valeria.work2022@gmail.com>
2023-11-03 19:39:22 +0200

    ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,53 @@
-import { FeedbackOptions }  from 'components/Feedback/Feedback';
+import { FeedbackOptions } from 'components/Feedback/Feedback';
 import Statistics2 from 'components/Statistics/Statistics2';
 import Section from 'components/Section/Section';
-import React, { Component } from "react";
+import React, { useState } from 'react';
 
-class App extends Component {
-    state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    };
-  
-  onLeaveFeedback = evt => { 
-    console.log(evt.target)
+const App = () => {
+  const [good, setGood] = useState(0);
+
+  const [neutral, setNeutral] = useState(0);
+
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = evt => {
+    console.log(evt.target);
+
     const { name } = evt.target;
-    this.setState(prevState => {
-      return { [name]: prevState[name] + 1 };
-    })
-  }
 
-  
-  render() {
-       const { good, neutral, bad } = this.state;
+    switch (name) {
+      case 'good':
+        setGood(good + 1);
+
+        break;
+
+      case 'neutral':
+        setNeutral(neutral + 1);
+
+        break;
+
+      case 'bad':
+        setBad(bad + 1);
+
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
-    <Section title="Please leave a feedback">
-      <FeedbackOptions
-  options={Object.keys(this.state)}
-  onLeaveFeedback={this.onLeaveFeedback}
-/>
+    <>
+      <Section title="Please leave a feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+
         <Statistics2 good={good} neutral={neutral} bad={bad} />
       </Section>
+    </>
   );
-  }
-}
+};
 
 export default App;
